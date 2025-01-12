4c086503c70eff69359373a0a418208ee773c278
isaias goncalves <isaiasgr575@gmail.com>
2025-01-12 00:35:35 -0300

    ajustando cors

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // importando as bibliotecas e funções
 import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
 import dotenv from "dotenv";
 import { criar_produtos } from "./functions/criar_produto.js";
 import { consultar_produtos } from "./functions/consultar_produtos.js";
@@ -9,7 +10,14 @@ import { todos_produtos } from "./functions/todos_produtos.js";
 
 // instanciando as funções
 dotenv.config()
+const PORT = process.env.PORT
 const app = fastify()
+
+// registrando o plugin cors
+app.register(fastifyCors, ({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+}))
 
 // criando as rotas para produtos
 app.post('/produtos/criar', criar_produtos)
@@ -20,8 +28,8 @@ app.delete('/produtos/:id', deletar_produtos)
 
 // configurando o servidor
 app.listen({
-  port: 5454,
+  port: PORT,
   host: '0.0.0.0'
 }, () => {
-  console.log('Servidor rodando na porta 5454')
+  console.log('Servidor funcionando')
 })
